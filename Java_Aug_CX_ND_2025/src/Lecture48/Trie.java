d4081c7113c7539ff169d998beff164a433add32
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2026-02-22 20:12:59 +0530

    Tries

---
+++ Java_Aug_CX_ND_2025/src/Lecture48/Trie.java
@@ -0,0 +1,64 @@
+package Lecture48;
+
+import java.util.HashMap;
+
+public class Trie {
+	public class Node {
+		Character ch;
+		HashMap<Character, Node> child = new HashMap<>();
+		boolean isEnd;
+	}
+
+	private Node root;
+
+	public Trie() {
+		Node nn = new Node();
+		nn.ch = '*';
+		root = nn;
+	}
+
+	public void insert(String word) {
+		Node curr = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (curr.child.containsKey(ch)) {
+				curr = curr.child.get(ch);
+			} else {
+				Node nn = new Node();
+				nn.ch = ch;
+				curr.child.put(ch, nn);
+				curr = nn;
+			}
+		}
+		curr.isEnd = true;
+	}
+
+	public boolean search(String word) {
+		Node curr = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (curr.child.containsKey(ch)) {
+				curr = curr.child.get(ch);
+			} else {
+				return false;
+			}
+		}
+		return curr.isEnd;
+	}
+
+	public boolean startsWith(String prefix) {
+		Node curr = root;
+
+		for (int i = 0; i < prefix.length(); i++) {
+			char ch = prefix.charAt(i);
+			if (curr.child.containsKey(ch)) {
+				curr = curr.child.get(ch);
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+}
